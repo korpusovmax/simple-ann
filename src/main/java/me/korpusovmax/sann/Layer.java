6e9b63b3465431013cf2fbf90186f3ec28601a54
Korpusov Maxim <65653941+korpusovmax@users.noreply.github.com>
2021-10-20 20:25:00 +0300

    Update Layer.java

--- src/main/java/me/korpusovmax/sann/Layer.java
+++ src/main/java/me/korpusovmax/sann/Layer.java
@@ -1,1 +1,59 @@
+package sann;
 
+public class Layer {
+	//layer type (default/recurrent)
+	public String type;
+	public double[] layer, errors;
+	
+	public Layer(int count) {
+		this.type = "default";
+		this.layer = new double[count];
+		this.errors = new double[count];
+		
+		for(int i = 0; i < count; i++) {
+			this.layer[i] = 0;
+		} 
+	}
+	public Layer(int count, String type) {
+		this.type = type;
+		this.layer = new double[count];
+		this.errors = new double[count];
+		
+		for(int i = 0; i < count; i++) {
+			this.layer[i] = 0;
+		}
+	} 
+	
+	//setup new neurons values
+	public void setNeurons(double[] n) {
+		layer = n;
+	} 
+	
+	//get first neuron value
+	public double getFirst() {
+		return layer[0];
+	}
+	//and last
+	public double getLast() {
+		return layer[layer.length-1];
+	} 
+	//get id of the most active neuron 
+	public int getOneHot() {
+		int ret = 0;
+		
+		for(int i = 1; i < layer.length; i++) {
+			if (layer[i] > layer[ret]) {
+				ret = i;
+			} 
+		}
+		return ret;
+	}
+	//get error
+	public double getError(double[] target) {
+		double err = 0.0d;
+		for(int i = 0; i < layer.length; i++) {
+			err = err + (target[i] - layer[i]);
+		} 
+		return err;
+	} 
+}
