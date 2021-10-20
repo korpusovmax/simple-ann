e8b32917b37c95acbdd715307971e158dea708c7
Korpusov Maxim <65653941+korpusovmax@users.noreply.github.com>
2021-10-20 20:24:33 +0300

    Update Net.java

--- src/main/java/me/korpusovmax/sann/Net.java
+++ src/main/java/me/korpusovmax/sann/Net.java
@@ -1,59 +1,114 @@
+/*java Simple Artificial Neural Network library
+by @maxcrocus (telegram)
+suggestions and support are welcome*/
+
 package sann;
 
-public class Layer {
-	//layer type (default/recurrent)
-	public String type;
-	public double[] layer, errors;
+import me.korpusovmax.sann.Layer;
+import me.korpusovmax.sann.Weights;
+import java.util.ArrayList;
+
+public class Net {
+	public ArrayList<Layer> layers;
+	public ArrayList<Weights> weights;
+	public double[][] trainingInput, trainingOutput;
 	
-	public Layer(int count) {
-		this.type = "default";
-		this.layer = new double[count];
-		this.errors = new double[count];
+	public Net(int inputLength, int hiddenLayers, int defaultLength, int outputLength) {
+		this.layers = new ArrayList<>();
+		this.layers.add(new Layer(inputLength));
 		
-		for(int i = 0; i < count; i++) {
-			this.layer[i] = 0;
+		for (int i = 0; i < hiddenLayers; i++) {
+			this.layers.add(new Layer(defaultLength));
+		}
+		
+		this.layers.add(new Layer(outputLength));
+	}
+	
+	public Net() {
+		this.layers = new ArrayList<>();
+	}
+	
+	//setup
+	public void addLayers(int count, int len) {
+		for (int i = 0; i < count; i++) {
+			layers.add(new Layer(len));
+		}
+	}
+	
+	public void addRecurrentLayers(int count, int len) {
+		for (int i = 0; i < count; i++) {
+			layers.add(new Layer(len, "recurrent"));
+		}
+	}
+	//weights
+	public void setup() {
+		weights = new ArrayList<>();
+		for(int i = 0; i < layers.size()-1; i++) {
+			weights.add(new Weights(layers.get(i), layers.get(i+1)));
 		} 
 	}
-	public Layer(int count, String type) {
-		this.type = type;
-		this.layer = new double[count];
-		this.errors = new double[count];
+	
+	//feed forward from one layer to other
+	public Layer forward(int layerId) {
+		int id = layerId-1; //previous layer
+		Weights weights = this.weights.get(id);
 		
-		for(int i = 0; i < count; i++) {
-			this.layer[i] = 0;
-		}
-	} 
-	
-	//setup new neurons values
-	public void setNeurons(double[] n) {
-		layer = n;
-	} 
-	
-	//get first neuron value
-	public double getFirst() {
-		return layer[0];
+		return weights.forward();
 	}
-	//and last
-	public double getLast() {
-		return layer[layer.length-1];
-	} 
-	//get id of the most active neuron 
-	public int getOneHot() {
-		int ret = 0;
-		
-		for(int i = 1; i < layer.length; i++) {
-			if (layer[i] > layer[ret]) {
-				ret = i;
-			} 
+	//one by one layers feed forward
+	public Layer forwardAll() {
+		Layer ret = layers.get(layers.size()-1);
+		for(int i = 1; i < weights.size()+1; i++) {
+			ret = forward(i);
 		}
 		return ret;
 	}
-	//get error
-	public double getError(double[] target) {
-		double err = 0.0d;
-		for(int i = 0; i < layer.length; i++) {
-			err = err + (target[i] - layer[i]);
-		} 
-		return err;
+	//back propogation for all layers, finding errors and setup new weights
+	public void backPropogation(double[] target, double k) {
+		weights.get(weights.size()-1).fixError(target);
+		
+		for(int i = weights.size()-1; i > 0; i--) {
+			weights.get(i).findError();
+		}
+		for(int i = weights.size()-1; i > - 1; i--) {
+			weights.get(i).backward(k);
+		}
 	} 
+	
+	//training
+	public void setTrainingData(double[][] inputData, double[][] outputData) {
+		trainingInput = inputData;
+		trainingOutput = outputData;
+	} 
+	
+	public void train(double k, int iterations) {
+		int iteration = 0;
+		while (iteration < iterations) {
+			layers.get(0).layer = trainingInput[iteration%trainingInput.length];
+			
+			forwardAll();
+			
+			backPropogation(trainingOutput[iteration%trainingOutput.length], k);
+			
+			if (trainingListener != null) { 
+				trainingListener.onTraining(iteration, layers.get(layers.size()-1).getError(trainingOutput[iteration%trainingOutput.length]));
+			}
+			iteration++;
+		}
+		
+		if (trainingListener != null) { 
+				trainingListener.onTrainingFinished(iteration, layers.get(layers.size()-1).getError(trainingOutput[iteration%trainingOutput.length]));
+		}
+	}
+	//listeners
+	private OnTrainingListener trainingListener;
+	
+	public void setOnTrainingListener(OnTrainingListener listener) {
+		trainingListener = listener;
+	}
+	
+	public interface OnTrainingListener {
+		void onTraining(int iteration, double error);
+		void onTrainingFinished(int iteration, double error);
+	}
 }
